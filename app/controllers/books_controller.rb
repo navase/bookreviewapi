2b73538f7b9236944e137c9810d1f13d7650402e
navase <margot.sauter@gmail.com>
2016-02-27 01:25:43 +0100

    Books controller + tests

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,33 @@
 class BooksController < ApplicationController
+  def index
+    render json: {
+      books: Book.order(:title)
+    }
+  end
+
+  def show
+    book = Book.find(params[:id])
+    render json: {
+      book: book
+    }
+  end
+
+  def create
+    if book = Book.create(book_params)
+    render json: {
+      book: book
+    }
+    else
+      render json: {
+        message: "Something went wrong with adding your book. Please try again!",
+        errors: book.errors
+      }
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author, :summary, :publication_date)
+  end
 end
--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe BooksController, type: :controller do
+  book = FactoryGirl.create(:book)
 
+  describe "GET #index" do
+    it "renders the :index" do
+      get :index
+      expect(response).to be_success
+    end
+
+    it "returns json" do
+      get :index
+      expect(response.header['Content-Type']).to include('application/json')
+    end
+  end
+
+  describe "GET #show" do
+    it "renders the :show" do
+      book
+      get :show, id: book.id
+      expect(response).to be_success
+    end
+  end
+
+  describe "POST #create" do
+    context "with valid attributes" do
+      it "saves the new book in the database" do
+        expect {
+          post :create, book: FactoryGirl.attributes_for(:book)
+        }.to change(Book, :count).by(1)
+      end
+    end
+
+    context "with invalid attributes" do
+      it "does not save the new book in the database" do
+        expect {
+          post :create, book: FactoryGirl.attributes_for(:book, title: nil)
+        }.to_not change(Book, :count)
+      end
+    end
+  end
 end
